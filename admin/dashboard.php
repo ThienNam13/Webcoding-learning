6c91b93dba96d1fe8361feeb0729142fef3f7276
TruongDuHoai <Hoaitd3404@ut.edu.vn>
2025-07-06 10:05:20 +0700

    them xem chi tiet

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-
+//require_once 'auth_check.php';
 // Kiểm tra quyền admin
 //if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
 //header("Location: login.php");
 //exit;
-//}
+//
 
 if (isset($_GET['action']) && $_GET['action']==='logout') {
   session_unset();
@@ -62,7 +62,6 @@ $adminName = $_SESSION['fullname'] ?? 'Admin';
       <a href="stats.php">Truy cập</a>
     </div>
   </div>
-
 <div id="toast" class="toast" style="display:none;"></div>
   <script>
     function showToast(msg){
--- admin/foods.php
+++ admin/foods.php
@@ -2,6 +2,7 @@
 <?php
 session_start();
 require_once("../php/db.php");
+require_once 'auth_check.php';
 
 // // Kiểm tra quyền admin
 // if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
@@ -131,7 +132,4 @@ $foods = $link->query("SELECT * FROM foods ORDER BY id DESC");
 <script src="assets/js/foods.js"></script>
 </body>
 </html>
-
-
-<?php require_once 'auth_check.php'; ?>
 
